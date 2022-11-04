6af90f28b0ebaafcc263858f04613c3b298f7278
Angélique <angelique.massolo@gmail.com>
2022-11-04 11:16:53 +0100

    fullyear

--- routes/year.js
+++ routes/year.js
@@ -5,7 +5,7 @@ const fetch = require("node-fetch");
 /* GET users listing. */
 router.get("/", (req, res)=> {
     let date = new Date();
-    let year=date.getYear()
+    let year=date.getFullYear()
 
   res.json({ year: year });
 });
